90bd4e6340340fcaf6bad1e69ce26ca795b87aa3
06mohan <120101642+06mohan@users.noreply.github.com>
2022-12-13 18:06:36 +0530

    calculation.js
    new code added

---
+++ calculation.js
@@ -0,0 +1,14 @@
+
+let	A = 10;
+let	B = 10;
+let	C = A+B;
+	 document.write("Add =" +C+ "</br>");
+let	D= A-B;
+	 document.write("sub =" +D+ "</br>");
+let	E = A*B;
+	 document.write("multi =" +E+ "</br>");
+let	F = A/B;
+	 document.write("div =" +F+ "</br>");
+
+	
+
